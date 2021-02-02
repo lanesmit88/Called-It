779c9fd8a475cdb4179733147f9ff6c33886c8d2
Lane Smit <lanesmit88@outlook.com>
2021-02-02 17:20:08 -0600

    messages seeder

--- backend/db/seeders/20210202180940-create-message-data.js
+++ backend/db/seeders/20210202180940-create-message-data.js
@@ -15,8 +15,44 @@ module.exports = {
       "Messages",
       [
         {
-          name: "John Doe",
-          isBetaMember: false,
+          text: "Hey cool page!",
+          senderId: 1,
+          recipientId: 2,
+        },
+        {
+          text: "We should hang sometime!",
+          senderId: 1,
+          recipientId: 5,
+        },
+        {
+          text: "You are soo smart:0",
+          senderId: 2,
+          recipientId: 1,
+        },
+        {
+          text: "You remind me of my uncle Jim",
+          senderId: 3,
+          recipientId: 1,
+        },
+        {
+          text: "How long have you been on this app?",
+          senderId: 3,
+          recipientId: 1,
+        },
+        {
+          text: "Heya",
+          senderId: 6,
+          recipientId: 7,
+        },
+        {
+          text: "How tall are you?",
+          senderId: 9,
+          recipientId: 16,
+        },
+        {
+          text: "Google me. Im rich ok",
+          senderId: 17,
+          recipientId: 9,
         },
       ],
       {}
