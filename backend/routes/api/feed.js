6c01602b95a6988371459ce6e89b83d7669159c3
Lane Smit <lanesmit88@outlook.com>
2021-02-03 12:25:10 -0600

    back-end complete

--- backend/routes/api/feed.js
+++ backend/routes/api/feed.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
-const { User, Follow } = require("../../db/models");
+const { Post, Follow } = require("../../db/models");
 
 router.get(
   "/:id",
   asyncHandler(async (req, res, next) => {
     userId = req.params.id;
-    const followed = await Follow.findAll({ where: { followedId: userId } });
-    console.log("---------------------------------------", followed)
-    res.json({followed: followed})
+    const followed = await Follow.findAll({ where: { followerId: userId } });
+    stuff = [];
+    followed.forEach((follow) => {
+      temp = follow.dataValues.followedId;
+      stuff.push(temp);
+    });
+
+    let feed = [];
+
+    await Promise.all(stuff.map(async (id) => {
+      const temp = await Post.findAll({ where: { userId: id } });
+      feed.push(temp);
+    }));
+    console.log("+++++++++++++++++++++++", feed)
+    res.json({ feed });
   })
 );
 
