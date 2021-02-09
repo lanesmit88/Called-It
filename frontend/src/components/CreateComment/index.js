95ecdbe4e2e4dd9a6af823c2802c4b4b213f872a
Lane Smit <lanesmit88@outlook.com>
2021-02-09 12:44:12 -0600

    front end create comment started

--- frontend/src/components/CreateComment/index.js
+++ frontend/src/components/CreateComment/index.js
@@ -1,7 +1,32 @@
-import "./index.css"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import "./index.css";
 
 function CreateComment() {
-return <button></button>
+  const dispatch = useDispatch();
+  function submitForm(e) {
+    const [text, setText] = useState("");
+    e.preventDefault();
+    dispatch(fetchCreateComment({ text, userId, postId }));
+  }
+
+  return (
+    <>
+      <form>
+        <button
+          id="agreeable-button"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          type="submit"
+        >
+          Add Comment
+        </button>
+        <textarea placeholder="Type here..."></textarea>
+      </form>
+    </>
+  );
 }
 
-export default CreateComment
+export default CreateComment;
