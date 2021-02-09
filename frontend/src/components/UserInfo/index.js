a2b78d2624c42b187588cfd080730bf012966534
Lane Smit <lanesmit88@outlook.com>
2021-02-09 15:08:36 -0600

    back-up user image

--- frontend/src/components/UserInfo/index.js
+++ frontend/src/components/UserInfo/index.js
@@ -1,16 +1,23 @@
 import "./index.css";
 
-
 function UserInfo({ profPhoto, username, userId }) {
-
   return (
     <div id="postUserInfo">
-      <img
-        id="profPhotoPost"
-        src={profPhoto}
-        alt="Not Found"
-      ></img>
-      <a href={`/profile/${userId}`} >{username}</a>
+      {profPhoto ? (
+        <>
+          <img id="profPhotoPost" alt="Not Found" src={profPhoto} />
+          <a href={`/profile/${userId}`}>{username}</a>
+        </>
+      ) : (
+        <>
+          <img
+            id="profPhotoPost"
+            alt="Not Found"
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          />
+          <a href={`/profile/${userId}`}>{username}</a>
+        </>
+      )}
     </div>
   );
 }
