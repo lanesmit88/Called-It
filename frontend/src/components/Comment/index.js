57c1c79ef2d15b7b7b34ce6e6b6549608c3bcc31
Lane Smit <lanesmit88@outlook.com>
2021-02-17 11:51:56 -0600

    delete comment front end button

--- frontend/src/components/Comment/index.js
+++ frontend/src/components/Comment/index.js
@@ -1,11 +1,24 @@
 import "./index.css";
 import { NavLink } from "react-router-dom";
 
+import { useDispatch } from "react-redux";
+import { fetchDeleteComment } from "../../store/comment";
+
+
 function Comment({ comment }) {
   const {
     text,
-    User: { id, username, profilePhotoUrl, bio },
+    id,
+    userId,
+    postId,
+    User: { username, profilePhotoUrl, bio },
   } = comment;
+  const dispatch = useDispatch();
+
+  function submitDeleteComment(e) {
+    e.preventDefault();
+    dispatch(fetchDeleteComment({ id, userId, postId }));
+  }
 
   return (
     <div id="comment">
@@ -13,6 +26,17 @@ function Comment({ comment }) {
         {username}
       </NavLink>
       <h3>{text}</h3>
+      <form onSubmit={submitDeleteComment}>
+        <button
+          id="delete-comment"
+          className="fas fa-times"
+          value={deleteComment}
+          onChange={(e) => {
+            setDeleteComment(e.target.value);
+          }}
+          type="submit"
+        ></button>
+      </form>
     </div>
   );
 }
