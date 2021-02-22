c9b29f0564a416f1ebfaeb7ad984a6ade6fb34b6
Lane Smit <lanesmit88@outlook.com>
2021-02-22 12:04:06 -0600

    started search component

---
+++ frontend/src/components/Search/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { searchDispatch } from "../../store/search";
+function Search() {
+  const dispatch = useDispatch();
+  const [searchText, setSearchText] = useState("");
+    const [focused, setFocused] = useState(false);
+  const search = useSelector((reduxState) => reduxState.search);
+
+  useEffect(() => {
+    dispatch(searchDispatch(searchText));
+  });
+
+  return (
+    <div className="search-wrapper">
+      <div className="search-box-container">
+        <button className={focused ? "search-button focused" : "search-button"}>
+          <i className="fa fa-search" />
+          <input
+            className="search-input"
+            onBlur={() => setFocused(false)}
+            onFocus={() => setFocused(true)}
+            type="text"
+            placeholder="Search Users"
+            value={searchText}
+            onChange={(event) => setSearchText(event.target.value)}
+          ></input>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
--- frontend/src/store/search.js
+++ frontend/src/store/search.js
@@ -1,25 +1,23 @@
 import { fetch } from "./csrf.js";
 
-const SEARCH_USERS = "search/searchUsers";
+const SEARCH = "search/searchResult";
 
-const loadUserResults = (users) => ({
-  type: SEARCH_USERS,
-  payload: users,
+const loadSearchResults = (search) => ({
+  type: SEARCH,
+  search: search,
 });
 
-export const searchUsers = (query) => async (dispatch) => {
-  const res = await fetch(`/api/search/users/${query}`);
+export const searchDispatch = (query) => async (dispatch) => {
+  const res = await fetch(`/api/search/${query}`);
   const users = res.data.users;
-  dispatch(loadUserResults(users));
+  dispatch(loadSearchResults(users));
 };
 
 function reducer(state = {}, action) {
   let newState;
   switch (action.type) {
-    case SEARCH_USERS:
-      newState = { ...state };
-      newState.users = action.payload;
-      return newState;
+    case SEARCH:
+      return action.search;
     default:
       return state;
   }
