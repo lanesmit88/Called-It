d5655ea3e2dd7ffc438e99ff3efcbc2f10312950
Lane Smit <lanesmit88@outlook.com>
2021-02-23 16:15:06 -0600

    footer links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,9 +2,31 @@ import "./index.css";
 
 function Footer() {
   return (
-    <>
-      <h1>sup</h1>
-    </>
+    <div id="footer-container">
+      <div id="footer-title">
+        <h1>Called It</h1>
+      </div>
+      <div id="footer-logo">
+        <h1>Called It</h1>
+      </div>
+      <div id="footer-socials">
+        <a href="https://github.com/lanesmit88" class="fab fa-github">
+          Github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lane-smit-724291203/"
+          class="fab fa-linkedin"
+        >
+          Linkedin
+        </a>
+        <a
+          href="https://www.linkedin.com/in/lane-smit-724291203/"
+          class="fab fa-linkedin"
+        >
+          Linkedin
+        </a>
+      </div>
+    </div>
   );
 }
 
