08ec1dd53762dec949940743feebc72fbb40317d
Lane Smit <lanesmit88@outlook.com>
2021-02-23 16:32:28 -0600

    going to add photo

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -7,23 +7,15 @@ function Footer() {
         <h1>Called It</h1>
       </div>
       <div id="footer-logo">
-        <h1>Called It</h1>
+        <img src="../../public/favicon.ico"></img>
       </div>
       <div id="footer-socials">
         <a href="https://github.com/lanesmit88" class="fab fa-github">
-          Github
         </a>
         <a
           href="https://www.linkedin.com/in/lane-smit-724291203/"
           class="fab fa-linkedin"
         >
-          Linkedin
-        </a>
-        <a
-          href="https://www.linkedin.com/in/lane-smit-724291203/"
-          class="fab fa-linkedin"
-        >
-          Linkedin
         </a>
       </div>
     </div>
