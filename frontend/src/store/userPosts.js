0da2917f7ccec1f164d80d3d6174d9b04eaf749a
Lane Smit <lanesmit88@outlook.com>
2021-03-04 22:27:54 -0600

    dropdown styling

--- frontend/src/store/userPosts.js
+++ frontend/src/store/userPosts.js
@@ -42,7 +42,6 @@ export const createPost = (body) => {
       method: "POST",
       body: JSON.stringify(body),
     });
-
     const newPost = res.data.createPost;
     dispatch(CreatePost(newPost));
   };
