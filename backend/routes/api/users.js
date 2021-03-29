a6468fc0bda9c526d18368337758409c36f0edc1
Lane Smit <lanesmit88@outlook.com>
2021-03-28 20:52:22 -0500

    done for the night

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { check } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const { singleMulterUpload, singlePublicFileUpload } = require("../awsS3");
+const { singleMulterUpload, singlePublicFileUpload } = require("../../awsS3");
 const { handleValidationErrors } = require("../../utils/validation");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User, Post, PostInteraction, Comment } = require("../../db/models");
